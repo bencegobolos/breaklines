41962546a4ca4537104b0904e7796c3873ae1395
Bence Gobolos <bence.gobolos@hightec-rt.com>
2016-03-18 12:23:23 +0100

    Outsource fileprops.py into HDP-1160.py, modified arguments logic.

--- HDP-1160.py
+++ HDP-1160.py
@@ -6,24 +6,33 @@ import subprocess
 import argparse
 import fnmatch
 
+
 # Get input: source directory of users guide.
 def get_input():
     desc = 'Improve your file aesthetics by breaking long lines.'
-    ap = argparse.ArgumentParser(description=desc)
-    ap.add_argument('-m', '--maxlen', help='the maximum length of a line '
-                                           '(100 on default)',
+    epilog = 'Script is made to check users-guide repository for long lines ' \
+             'and break them if possible.'
+    ap = argparse.ArgumentParser(description=desc, epilog=epilog)
+    ap.add_argument('-m', '--maxlen',
+                    help='the maximum length of a line (100 on default)',
                     type=int, default=100, metavar='LENGTH')
-    group = ap.add_mutually_exclusive_group()
-    group.add_argument('--srcdir', help='OVERWRITES users-guide repo .tex files')
-    group.add_argument('-f', '--file', help='the .tex file you want to print out formatted',
-                    type=file, metavar='TEXFILE')
-    group.add_argument('-c', '--check', help='check if file contains long lines',
-                    type=file, metavar='TEXFILE')
+    ap.add_argument('-c', '--check',
+                    help='check if file or dir contains long lines',
+                    action='store_true')
+    ap.add_argument('input', help='Print out a .tex file formatted '
+                                  'or OVERWRITE a folder\'s .tex files.',
+                    metavar='INPUT')
+    try:
+        return ap.parse_args()
 
-    return ap.parse_args()
+    except:
+        ap.print_help()
+        sys.exit(-1)
 
 
-def checker(content, maxlen):
+# Check if a file contains long lines.
+# Print out to stdout the affected lines' numbers.
+def file_checker(content, maxlen):
     line_num = 1
     long_found = False
 
@@ -38,6 +47,19 @@ def checker(content, maxlen):
         print('[!]\t' + 'There is no long lines in your file.')
 
 
+# Checks a file's longest line.
+# Returns with the number of chars of the longest line.
+def dir_checker(content, maxlen):
+    maxllen = 0
+
+    for line in content:
+        llen = len(line)
+        if (llen > maxllen):
+            maxllen = llen
+
+    return maxllen
+
+
 # Find .tex files in folder srcdir.
 # Returns:
 # * texfiles: list, contains the relative paths to .tex files.
@@ -49,6 +71,14 @@ def find_texfiles(srcdir):
             texfiles.append(os.path.join(root, file))
 
     return texfiles
+
+
+# Read file from file's path.
+def read_file(path):
+    with open(path) as f:
+        content = f.readlines()
+
+    return content
 
 
 # Output content(param 1) into file(param 2)
@@ -78,23 +108,30 @@ def confirm_srcdir():
 def main():
     args = get_input()
 
-    if args.check:
-        content = args.check.readlines()
-        checker(content, args.maxlen)
-        sys.exit(0)
-
     script_path = os.path.dirname(os.path.abspath(__file__))
     command = ['python']
     command.append(os.path.join(script_path, 'breaklines.py'))
     command.append(str(args.maxlen))
 
-    if args.srcdir:
+    # Input is a directory.
+    if os.path.isdir(args.input):
+        texfiles = find_texfiles(args.input)
+
+        if args.check:
+            for file in texfiles:
+                content = read_file(file)
+                maxllen = dir_checker(content, args.maxlen)
+                if args.maxlen < maxllen:
+                    print(file)
+                    print("\tMax length: " + str(maxllen) + '\n')
+            sys.exit(0)
+
+        # Confirm if user wants to overwrite .tex files.
         execute = confirm_srcdir()
         if not execute:
             print('Script aborted.')
             sys.exit(-1)
 
-        texfiles = find_texfiles(args.srcdir)
         for file in texfiles:
             command.append(file)
             content = subprocess.check_output(command)
@@ -102,13 +139,19 @@ def main():
             dump(content, file)
         sys.exit(0)
 
-    if args.file:
-        command.append(str(args.file.name))
+    # Input is a file.
+    elif os.path.isfile(args.input):
+        if args.check:
+            content = read_file(args.input)
+            file_checker(content, args.maxlen)
+            sys.exit(0)
+        command.append(args.input)
         subprocess.call(command)
         sys.exit(0)
 
     print('\nScript is made to check users-guide repository for long lines.')
     print('Use -h or --help option to get more info.\n')
+    sys.exit(-1)
 
 
 if __name__ == "__main__":
